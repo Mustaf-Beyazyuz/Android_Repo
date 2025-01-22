681e14f8e07b0a0766cd4d567a1283f0c39dfd47
Mustaf-Beyazyuz <119809831+Mustaf-Beyazyuz@users.noreply.github.com>
2025-01-22 14:13:55 +0300

    Basic_Activity

--- 006-MultipleActivity/app/src/main/java/com/example/android/app/multipleactivity/viewModel/PaymentInfo.kt
+++ 006-MultipleActivity/app/src/main/java/com/example/android/app/multipleactivity/viewModel/PaymentInfo.kt
@@ -2,6 +2,12 @@ package com.example.android.app.multipleactivity.viewModel
 
 data class PaymentInfo(var name: String = "", var unitprice : Double = 0.0 , var quantity : Int)
 {
-
+    fun reset() {
+        run {
+            name = ""
+            unitprice = 0.0
+            quantity = 0
+        }
+    }
     override fun toString() =  "$name,$unitprice * $quantity = ${unitprice * quantity} "
 }
