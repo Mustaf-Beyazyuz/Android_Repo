6bbf0d69f53d662134c79a91f1aa91575215df09
MustafaBeyazyuz <mustafa.beyazyuzz@gmail.com>
2025-02-18 15:41:38 +0300

    007-HiltApp

--- 007-HiltApp/app/src/main/java/com/mustafabeyazyuz/android/hilt/datatime/DateTimeInfo.kt
+++ 007-HiltApp/app/src/main/java/com/mustafabeyazyuz/android/hilt/datatime/DateTimeInfo.kt
@@ -2,9 +2,6 @@ package com.mustafabeyazyuz.android.hilt.datatime
 
 import com.mustafabeyazyuz.android.hilt.datatime.annotation.LocalDateTimeInterceptor
 import com.mustafabeyazyuz.module.formatter.annotation.DateTimeFormatterInterceptor
-import dagger.hilt.android.scopes.ActivityScoped
-import hilt_aggregated_deps._com_mustafabeyazyuz_module_formatter_DateTimeFormatterModule
-import java.lang.reflect.Constructor
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import javax.inject.Inject
