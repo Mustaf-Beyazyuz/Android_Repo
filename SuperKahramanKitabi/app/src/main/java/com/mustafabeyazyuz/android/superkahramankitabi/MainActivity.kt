a1f9c8b0a0452388edce9105879c8495ea1a69e5
Mustaf-Beyazyuz <119809831+Mustaf-Beyazyuz@users.noreply.github.com>
2025-03-27 09:22:19 +0300

    SuperKahramanKitabi

--- SuperKahramanKitabi/app/src/main/java/com/mustafabeyazyuz/android/superkahramankitabi/MainActivity.kt
+++ SuperKahramanKitabi/app/src/main/java/com/mustafabeyazyuz/android/superkahramankitabi/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.mustafabeyazyuz.android.superkahramankitabi.databinding.ActivityMainBinding
 import java.util.ArrayList
 
@@ -14,8 +15,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var superKahramanListesi : ArrayList<SuperKahraman>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+                enableEdgeToEdge()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -27,5 +32,11 @@ class MainActivity : AppCompatActivity() {
         val ironman = SuperKahraman("Ironman","Patron", R.drawable.ironman)
         val captanAmerica = SuperKahraman("Captan America", "Asker",R.drawable.captan_america)
         superKahramanListesi = arrayListOf(superman,batman,ironman,captanAmerica)
+
+
+
+        val adapter = SuperKahramanAdapter(superKahramanListesi)
+        binding.superKahramanRecylerView.layoutManager = LinearLayoutManager(this)
+        binding.superKahramanRecylerView.adapter = adapter
     }
 }
--- SuperKahramanKitabi/app/src/main/java/com/mustafabeyazyuz/android/superkahramankitabi/SuperKahramanAdapter.kt
+++ SuperKahramanKitabi/app/src/main/java/com/mustafabeyazyuz/android/superkahramankitabi/SuperKahramanAdapter.kt
@@ -1,4 +1,24 @@
 package com.mustafabeyazyuz.android.superkahramankitabi
 
-class SuperKahramanAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.mustafabeyazyuz.android.superkahramankitabi.databinding.RecylerRowBinding
+import java.util.ArrayList
+
+class SuperKahramanAdapter (val superKahramanListesi : ArrayList<SuperKahraman>) : RecyclerView.Adapter<SuperKahramanAdapter.superKahramanViewHolder>(){
+    class superKahramanViewHolder(val  binding : RecylerRowBinding): RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): superKahramanViewHolder {
+        val binding = RecylerRowBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return superKahramanViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return superKahramanListesi.size
+    }
+
+    override fun onBindViewHolder(holder: superKahramanViewHolder, position: Int) {
+        holder.binding.textViewRecylerView.text = superKahramanListesi[position].isim
+    }
 }
